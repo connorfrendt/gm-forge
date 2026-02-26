3ff33a61d8f58f160993b5afd9d8b2093bd9146f
Connor Frendt <42262048+connorfrendt@users.noreply.github.com>
2026-02-25 17:06:49 -0800

    adding in authentication

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -4,13 +4,18 @@
             <div>
                 <div>Sign Up</div>
                 <div>Name</div>
-                <input />
+                <input v-model="name" />
+                
                 <div>Email</div>
-                <input />
+                <input v-model="email" />
+                
                 <div>Password</div>
-                <input type="password" />
+                <input v-model="password" type="password" />
+                
                 <div>Confirm Password</div>
-                <input type="password" />
+                <input v-model="passwordConfirm" type="password" />
+                
+                <div class="submit-button" @click="signup">Submit</div>
             </div>
             <div>
                 <div>Log In</div>
@@ -18,11 +23,38 @@
                 <input />
                 <div>Password</div>
                 <input type="password" />
+                <div class="submit-button">Submitsss</div>
             </div>
         </div>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import pb from '../lib/pocketbase';
+
+const name = ref('');
+const email = ref('');
+const password = ref('');
+const passwordConfirm = ref('');
+
+const signup = async () => {
+    try {
+        const data = {
+            name: name.value,
+            email: email.value,
+            password: password.value,
+            passwordConfirm: passwordConfirm.value,
+        }
+
+        await pb.collection('users').create(data);
+
+        console.log('User created successfully');
+    }
+    catch (err) {
+        console.error('Signup error: ', err);
+    }
+}
+</script>
 
 <style scoped></style>
